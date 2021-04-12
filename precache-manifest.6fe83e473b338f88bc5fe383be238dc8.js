aa58c2340bf9182980d21b5d053616d94e77b070
Abhishek Tagline <abheeshek.tagline@gmail.com>
2021-04-12 18:12:01 +0530

    Updates

---
+++ precache-manifest.6fe83e473b338f88bc5fe383be238dc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "750245a2c553f25f4db2a37352bf331f",
+    "url": "/guguloan/index.html"
+  },
+  {
+    "revision": "7031ea526b8a7a2dc517",
+    "url": "/guguloan/static/css/main.74f71929.chunk.css"
+  },
+  {
+    "revision": "914d6e092a75f2ed7ac8",
+    "url": "/guguloan/static/js/2.b46fb681.chunk.js"
+  },
+  {
+    "revision": "dee17da52698493c2123d65df858f4ce",
+    "url": "/guguloan/static/js/2.b46fb681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7031ea526b8a7a2dc517",
+    "url": "/guguloan/static/js/main.3bd45b38.chunk.js"
+  },
+  {
+    "revision": "52ea9f31c7d84df8d4fc",
+    "url": "/guguloan/static/js/runtime-main.14803a95.js"
+  }
+]);
